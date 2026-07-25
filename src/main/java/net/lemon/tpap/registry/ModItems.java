acdd3a769d5b80e378465f5659d15840e60905df
shinycosmic <razorrotom@gmail.com>
2026-07-25 00:12:11 -0400

    Add Geckolib

--- src/main/java/net/lemon/tpap/registry/ModItems.java
+++ src/main/java/net/lemon/tpap/registry/ModItems.java
@@ -11,11 +11,11 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, TPAP.MODID);
 
     //Add Items below
-    public static final RegistryObject<Item> SMALL_MOLD = ITEMS.register("small_mold", () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> MEDIUM_MOLD = ITEMS.register("medium_mold", () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> LARGE_MOLD = ITEMS.register("large_mold", () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> HUGE_MOLD = ITEMS.register("huge_mold", () -> new Item(new Item.Properties()));
-    public static final RegistryObject<Item> PLANE_MOLD = ITEMS.register("plane_mold", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> SMALL_MOLD = ITEMS.register("small_mold", () -> new Item(new Item.Properties().durability(100)));
+    public static final RegistryObject<Item> MEDIUM_MOLD = ITEMS.register("medium_mold", () -> new Item(new Item.Properties().durability(100)));
+    public static final RegistryObject<Item> LARGE_MOLD = ITEMS.register("large_mold", () -> new Item(new Item.Properties().durability(100)));
+    public static final RegistryObject<Item> HUGE_MOLD = ITEMS.register("huge_mold", () -> new Item(new Item.Properties().durability(100)));
+    public static final RegistryObject<Item> PLANE_MOLD = ITEMS.register("plane_mold", () -> new Item(new Item.Properties().durability(100)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
