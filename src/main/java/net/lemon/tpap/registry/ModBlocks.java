b7d61fb462bf4b8ea59ac4b1588e757a040dae83
FunnyJamie <130430045+FunnyJamie@users.noreply.github.com>
2026-08-29 16:32:10 +1000

    desmodus

--- src/main/java/net/lemon/tpap/registry/ModBlocks.java
+++ src/main/java/net/lemon/tpap/registry/ModBlocks.java
@@ -36,6 +36,8 @@ public class ModBlocks {
     //Add Statues below: registerStatue(statueId, genus, poseCount, proppable, moldSize)
     public static final RegistryObject<StatueBlock> TRICERATOPS_HORRIDUS = registerStatue("triceratops_horridus", "triceratops", 5, false, MoldSize.LARGE);
     public static final RegistryObject<StatueBlock> TARBOSAURUS_BATAAR = registerStatue("tarbosaurus_bataar", "tarbosaurus", 5, true, MoldSize.HUGE);
+    public static final RegistryObject<StatueBlock> DESMODUS_DRACULAE = registerStatue("desmodus_draculae", "desmodus", 5, true, MoldSize.SMALL);
+
 
 
 
@@ -51,7 +53,10 @@ public class ModBlocks {
                         .mapColor(MapColor.STONE)
                         .sound(SoundType.STONE)
                         .strength(1.5F, 6.0F)
-                        .noOcclusion()));
+                        .noOcclusion()
+                        .noCollission()
+                ));
+
         RegistryObject<Item> item = ModItems.ITEMS.register(statueId, () -> new BlockItem(block.get(), new Item.Properties()));
         STATUES.add(block);
         STATUE_ITEMS.add(item);
